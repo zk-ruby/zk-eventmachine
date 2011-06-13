66c9d30d902cef4acd7f8a17d79b2de4be97bf74
slyphon <slyphon@gmail.com>
2011-06-13 03:21:19 +0000

    basic event delivery specs

--- lib/z_k/z_k_event_machine.rb
+++ lib/z_k/z_k_event_machine.rb
@@ -16,7 +16,7 @@ $LOAD_PATH.unshift(File.expand_path('../..', __FILE__)).uniq!
 require 'z_k/z_k_event_machine/iterator'
 require 'z_k/z_k_event_machine/deferred'
 require 'z_k/z_k_event_machine/callback'
-require 'z_k/z_k_event_machine/event_handler_proxy'
+require 'z_k/z_k_event_machine/event_handler_e_m'
 require 'z_k/z_k_event_machine/unixisms'
 require 'z_k/z_k_event_machine/client'
 
--- spec/z_k/z_k_event_machine/event_handler_e_m_spec.rb
+++ spec/z_k/z_k_event_machine/event_handler_e_m_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 module ZK::ZKEventMachine
   describe 'EventHandlerEM' do
     include EventedSpec::SpecHelper
@@ -20,7 +22,79 @@ module ZK::ZKEventMachine
     # this is a test of event delivery in general, not just of the
     # EventHandlerEM implementation
 
+    describe 'data event' do
+      include EventedSpec::EMSpec
 
+      before do
+        @path = "#{@base_path}/blah"
+        @data = "this is data"
+        @new_data = "this is other data"
+
+        @child_path = [@path, 'child'].join('/')
+      end
+
+      it %[should call the callback when the data of a watched node changes] do
+        @zkem.connect do
+          @zkem.event_handler.register(@path) do |event|
+            EM.reactor_thread?.should be_true
+            event.should be_node_changed
+            @zkem.close! { done }
+          end
+
+          @zkem.create(@path, @data) do |exc|
+            raise exc if exc
+
+            @zkem.stat(@path, :watch => true) do |e,*a| 
+              raise e if e
+
+              @zkem.set(@path, @new_data) { |e| raise e if e }
+            end
+          end
+        end
+      end
+
+      it %[should call the callback when the children of the watched node change] do
+        @zkem.connect do
+          @zkem.event_handler.register(@path) do |event|
+            EM.reactor_thread?.should be_true
+            event.should be_node_child
+            @zkem.close! { done }
+          end
+
+          eb_raise = lambda { |e| raise e if e }
+
+          @zkem.create(@path, @data).callback { |*|
+            @zkem.children(@path, :watch => true).callback { |ary|
+              ary.should be_empty
+
+              @zkem.create(@child_path, '') { |p|
+                p.should == @path
+
+              }.errback(&eb_raise)
+            }.errback(&eb_raise)
+          }.errback(&eb_raise)
+        end
+      end # it
+
+      it %[should call back the registered block when the node is deleted] do
+        @zkem.connect do
+          @zkem.event_handler.register(@path) do |event|
+            EM.reactor_thread?.should be_true
+            event.should be_node_deleted
+            @zkem.close! { done }
+          end
+
+          eb_raise = lambda { |e| raise e if e }
+
+          @zkem.create(@path, @data).callback do |*|
+            @zkem.stat(@path, :watch => true).callback do |*|
+              @zkem.delete(@path).errback(&eb_raise)
+
+            end.errback(&eb_raise)
+          end.errback(&eb_raise)
+        end
+      end
+    end
   end
 end
 
