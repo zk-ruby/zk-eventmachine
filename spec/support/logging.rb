c492647d0886c62409b6c4ddcb3f286d834de3b0
Jonathan D. Simms <slyphon@gmail.com>
2012-04-11 14:43:13 +0000

    fix up, comment spec logging support file

--- spec/support/logging.rb
+++ spec/support/logging.rb
@@ -1,13 +1,18 @@
 require 'logger'
 
-logger = Logger.new(File.expand_path('../../../test.log', __FILE__)).tap {|l| l.level = Logger::DEBUG}
+log_file = File.open(File.expand_path('../../../test.log', __FILE__), 'a').tap { |f| f.sync = true }
 
+Logger.new(log_file).tap do |log| 
+  log.level = Logger::DEBUG 
+  ZK.logger = log
+  Zookeeper.logger = log
+end
+
+# for debugging along with C output uncomment the following
+#
+# $stderr.sync = true
 # logger = Logger.new($stderr).tap { |l| l.level = Logger::DEBUG }
 # Zookeeper.set_debug_level(4)
-
-ZK.logger = logger
-Zookeeper.logger = ZK.logger
-
 
 def logger
   ZK.logger
